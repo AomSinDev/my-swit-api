10e8d132db711bca8bdab51ec87dc122f5717976
AomSinDev <aomsin17978@gmail.com>
2026-04-27 16:13:16 +0700

    Add files via upload

--- api-gateway.js
+++ api-gateway.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
